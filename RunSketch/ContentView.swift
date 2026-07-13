54c065a90ca26238efb6e305924bb74ea85c2f97
s4205700 <s4205700@student.rmit.edu.au>
2026-07-13 19:44:42 +1000

    SPRINT 1 - START, STOP AND TOGGLING IN BTWN

--- RunSketch/ContentView.swift
+++ RunSketch/ContentView.swift
@@ -8,12 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isRunning = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, Satvik!")
+        VStack(spacing: 20) {
+            
+            Image(systemName: "figure.run")
+                .font(.system(size: 60))
+            
+            Text("RunSketch")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            Text("Turning every run into a visual story.")
+                .multilineTextAlignment(.center)
+                .foregroundStyle(.secondary)
+            
+            if isRunning {
+                Text("Run in Progress!!")
+                
+            }
+            
+            Button(isRunning ? "Stop Run" : "Start Run") {
+                isRunning.toggle()
+                
+            }
+            .buttonStyle(.borderedProminent)
+            
         }
         .padding()
     }
