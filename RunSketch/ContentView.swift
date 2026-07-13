bfd835859150b56508c9192a2ecd5af5feb3eb69
s4205700 <s4205700@student.rmit.edu.au>
2026-07-13 14:55:02 +1000

    Initial Commit

---
+++ RunSketch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RunSketch
+//
+//  Created by Satvik Kharbanda on 13/7/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RunSketch/RunSketchApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunSketchApp.swift
+//  RunSketch
+//
+//  Created by Satvik Kharbanda on 13/7/2026.
+//
+
+import SwiftUI
+
+@main
+struct RunSketchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
